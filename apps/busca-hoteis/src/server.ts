282c2d78b5e90cdf0087f98c49dddecc66573326
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2024-04-25 21:21:41 -0300

    feat: get rooms

--- apps/busca-hoteis/src/server.ts
+++ apps/busca-hoteis/src/server.ts
@@ -111,6 +111,63 @@ app.get("/hotels", async (req: FastifyRequest, res: FastifyReply) => {
   });
 });
 
+const querySchemaRooms = z.object({
+  startDate: z.string().date(),
+  endDate: z.string().date(),
+});
+
+const paramsSchema = z.object({
+  hotelId: z.string(),
+});
+
+app.get("/rooms/:hotelId", async (req: FastifyRequest, res: FastifyReply) => {
+  const query = querySchemaRooms.safeParse(req.query);
+  const params = paramsSchema.safeParse(req.params);
+
+  if (query.success !== true) {
+    return res.status(422).send({
+      errors: query.error.flatten(),
+    });
+  }
+
+  if (params.success !== true) {
+    return res.status(422).send({
+      errors: params.error.flatten(),
+    });
+  }
+
+  const { endDate, startDate } = query.data;
+  const { hotelId } = params.data;
+
+  const roomsQuery = await db
+    .select({
+      id: rooms.id,
+    })
+    .from(rooms)
+    .innerJoin(reservations, eq(rooms.id, reservations.roomId))
+    .where(
+      and(
+        eq(rooms.hotelId, hotelId),
+        notInArray(
+          rooms.id,
+          db
+            .select({ id: rooms.id })
+            .from(reservations)
+            .innerJoin(
+              rooms,
+              and(
+                eq(rooms.id, reservations.id),
+                between(reservations.startDate, startDate, endDate),
+                between(reservations.endDate, startDate, endDate)
+              )
+            )
+        )
+      )
+    );
+
+  return res.send(roomsQuery);
+});
+
 app.listen({
   port: 3000,
   host: "0.0.0.0",
---
+++ script.sql
@@ -0,0 +1,31 @@
+CREATE TABLE IF NOT EXISTS "hotels" (
+	"id" varchar(255) PRIMARY KEY NOT NULL,
+	"name" varchar(100) NOT NULL,
+	"city" varchar(100) NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "reservations" (
+	"id" varchar(255) PRIMARY KEY NOT NULL,
+	"room_id" varchar(255) NOT NULL,
+	"start_date" date NOT NULL,
+	"end_date" date NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "rooms" (
+	"id" varchar(255) PRIMARY KEY NOT NULL,
+	"hotel_id" varchar(255) NOT NULL
+);
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "reservations" ADD CONSTRAINT "reservations_room_id_rooms_id_fk" FOREIGN KEY ("room_id") REFERENCES "rooms"("id") ON DELETE cascade ON UPDATE cascade;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "rooms" ADD CONSTRAINT "rooms_hotel_id_hotels_id_fk" FOREIGN KEY ("hotel_id") REFERENCES "hotels"("id") ON DELETE cascade ON UPDATE cascade;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
